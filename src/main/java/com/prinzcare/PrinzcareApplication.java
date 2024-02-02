338434b0f8054765d51a1010d7e19f64c858a352
apple <apple@Perandys-MacBook-Pro.local>
2024-02-02 16:46:07 +0100

    Initial working fronted with Backend

---
+++ src/main/java/com/prinzcare/PrinzcareApplication.java
@@ -0,0 +1,13 @@
+package com.prinzcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinzcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinzcareApplication.class, args);
+	}
+
+}
